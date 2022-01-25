95267a8bdbc3263c8636b9102a7b205512c30236
ccetsii <ccetsii@ull.edu.es>
2022-01-25 12:07:30 +0000

    source

--- no-ansible-scripts/install-podman-ubuntu18.04.sh
+++ no-ansible-scripts/install-podman-ubuntu18.04.sh
@@ -5,7 +5,7 @@
 
 
 apt-get install curl wget gnupg2 -y
-source /etc/os-release
+. /etc/os-release
 echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
 apt-get update -qq -y
