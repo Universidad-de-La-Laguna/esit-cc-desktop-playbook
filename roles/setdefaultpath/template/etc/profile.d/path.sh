a8988e94a8a34ef09939532407b931f9f56a3aa6
Benito Cuesta <bcuestav@ull.edu.es>
2023-11-22 09:47:57 +0000

    fix: cambiar path por defecto para Java 11

--- roles/setdefaultpath/template/etc/profile.d/path.sh
+++ roles/setdefaultpath/template/etc/profile.d/path.sh
@@ -1,4 +1,4 @@
-export PATH=/opt/node-v16.19.1-linux-x64/bin:/opt/jdk1.8.0_20/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export PATH=/opt/node-v16.19.1-linux-x64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-export JAVA_HOME=/opt/jdk1.8.0_20
-export CLASSPATH="/opt/jdk1.8.0_20/lib:."
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export CLASSPATH="/usr/lib/jvm/java-11-openjdk-amd64/lib:."
