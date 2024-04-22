d922644ddaebc8245dedd4611cde053028b5ecbd
ccetsii <ccetsii@ull.edu.es>
2024-04-22 11:09:05 +0000

    unset JAVA_HOME

--- roles/setdefaultpath/template/etc/profile.d/path.sh
+++ roles/setdefaultpath/template/etc/profile.d/path.sh
@@ -1,4 +1,5 @@
 export PATH=/opt/node-v16.19.1-linux-x64/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+#export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export CLASSPATH="/usr/lib/jvm/java-11-openjdk-amd64/lib:."
+
