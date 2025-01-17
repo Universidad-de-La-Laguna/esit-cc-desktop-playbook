faaa51187430a490a8b24206250e1eef4b27ea78
Pedro Gonzalez <pgonyan@gmail.com>
2025-01-17 12:33:44 +0000

    typo

--- agente-it-inventary/scripts/windows/software_info.py
+++ agente-it-inventary/scripts/windows/software_info.py
@@ -41,7 +41,7 @@ def get_is_vivado_20023_installed():
 
 python_dependencies = python_dependencies.replace('"', '\\"')
 
-installed_software = get_installed_software()
+installed_software = get_is_vivado_20023_installed()
 
 is_vivado_20023_installed = get_is_vivado_20023_installed()
 
