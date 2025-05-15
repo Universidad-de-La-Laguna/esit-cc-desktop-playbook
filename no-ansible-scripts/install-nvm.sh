90aec66939bbdcc6406b8b6fece9449a7929a419
Pedro Gonzalez <pgonyan@gmail.com>
2025-05-15 12:15:59 +0100

    Fix: Update install-nvm.sh to include installation of vscode-fixed and create symlink

--- no-ansible-scripts/install-nvm.sh
+++ no-ansible-scripts/install-nvm.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # This script installs nvm (Node Version Manager) and Node.js on a Linux system.
 
+cd /opt
+wget http://cc.etsii.ull.es/ftp/packages/vscode-fixed.tar.gz 
+tar -xvzf vscode-fixed.tar.gz
+ln -s  /opt/vscode-fixed/code-restringido /usr/local/bin/code-restringido
+
 rm -f /etc/profile
 cd /etc/
 wget https://raw.githubusercontent.com/Universidad-de-La-Laguna/esit-cc-desktop-playbook/refs/heads/master/no-ansible-scripts/profile
@@ -17,3 +22,5 @@ nvm install v20.11.1
 npm install -g typescript
 nvm alias default node
 nvm use default
+
+
