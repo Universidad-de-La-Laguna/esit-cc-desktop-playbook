0b7617681ed3164864b2e60f80dfa1b62bab67d3
Pedro Gonzalez <pgonyan@gmail.com>
2025-05-15 10:44:42 +0100

    Fix: Update install-nvm.sh to ensure profile is fetched and permissions are set correctly

--- no-ansible-scripts/install-nvm.sh
+++ no-ansible-scripts/install-nvm.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # This script installs nvm (Node Version Manager) and Node.js on a Linux system.
 
+rm -f /etc/profile
+cd /etc/
+wget https://raw.githubusercontent.com/Universidad-de-La-Laguna/esit-cc-desktop-playbook/refs/heads/master/no-ansible-scripts/profile
+chmod a+r /etc/profile
 
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | NVM_DIR=/usr/local/nvm bash
