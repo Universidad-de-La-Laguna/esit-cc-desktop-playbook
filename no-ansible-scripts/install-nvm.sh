3ea27f9b60593f39ee5843a53c3d3e2e9019476a
Pedro Gonzalez <pgonyan@gmail.com>
2025-05-15 09:38:55 +0100

    Fix: Ensure proper installation command for nvm in install-nvm.sh and main.yml

--- no-ansible-scripts/install-nvm.sh
+++ no-ansible-scripts/install-nvm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script installs nvm (Node Version Manager) and Node.js on a Linux system.
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | NVM_DIR=/usr/local/nvm bash
 chmod -R a+rw /usr/local/nvm
 nvm install v20.11.1
 npm install -g typescript
