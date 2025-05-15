7e6b2ed6b19add164cfd97aa48d852248cf086b7
Pedro Gonzalez <pgonyan@gmail.com>
2025-05-15 10:51:00 +0100

    fix

--- no-ansible-scripts/install-nvm.sh
+++ no-ansible-scripts/install-nvm.sh
@@ -9,7 +9,10 @@ chmod a+r /etc/profile
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | NVM_DIR=/usr/local/nvm bash
 chmod -R a+rw /usr/local/nvm
-NVM_DIR=/usr/local/nvm
+export NVM_DIR=/usr/local/nvm
+source /usr/local/nvm/nvm.sh
+
+
 nvm install v20.11.1
 npm install -g typescript
 nvm alias default node
