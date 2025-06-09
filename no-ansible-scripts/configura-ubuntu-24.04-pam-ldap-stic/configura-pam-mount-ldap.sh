09faac5f10914bfe8176e7cabfed8028178bbe56
Pedro Gonzalez <pgonyan@gmail.com>
2025-06-09 10:41:17 +0100

    configura-pam

--- no-ansible-scripts/configura-ubuntu-24.04-pam-ldap-stic/configura-pam-mount-ldap.sh
+++ no-ansible-scripts/configura-ubuntu-24.04-pam-ldap-stic/configura-pam-mount-ldap.sh
@@ -6,8 +6,6 @@ apt-get install -y libpam-ldap ldap-auth-client ldap-auth-config libnss-ldapd ld
 #chown root.root /etc/sssd/sssd.conf
 #chmod go-r  /etc/sssd/sssd.conf
 service sssd restart
-cd /soft/configura-ubuntu-pam-ldap-stic
-
 cd files
 rsync -av . /
 chmod 0600 /etc/sssd/sssd.conf
