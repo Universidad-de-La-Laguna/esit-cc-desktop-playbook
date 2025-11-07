c77c5fdb42284ba82b1dc162bd99e85958451ecd
Pedro Gonzalez <pgonyan@gmail.com>
2025-11-07 10:26:12 +0000

    Add installation script for MediaPipe and dependencies

---
+++ no-ansible-scripts/install-mediapipe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update -y
+apt install -y python3.12-dev python3.12-venv
+cd /opt
+python3.12 -m venv env-sipc
+source /opt/env-sipc/bin/activate
+python3.12 -m pip install --upgrade pip
+python3.12 -m pip install mediapipe pymunk pygame
+chmod a+w -R /opt/env-sipc
