fe7a7272ec40fbef1770db824f4d221e082219fd
Pedro Gonzalez <pgonyan@gmail.com>
2026-03-13 09:18:08 +0000

    menos verbose

--- no-ansible-scripts/install-code-restringido.sh
+++ no-ansible-scripts/install-code-restringido.sh
@@ -16,19 +16,19 @@ ln -s  /opt/vscode-fixed/code-restringido /usr/local/bin/code-restringido
 
 rm -f /etc/profile
 cd /etc/
-wget https://raw.githubusercontent.com/Universidad-de-La-Laguna/esit-cc-desktop-playbook/refs/heads/master/no-ansible-scripts/profile
+wget -q https://raw.githubusercontent.com/Universidad-de-La-Laguna/esit-cc-desktop-playbook/refs/heads/master/no-ansible-scripts/profile
 chmod a+r /etc/profile
 
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | NVM_DIR=/usr/local/nvm bash
+curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | NVM_DIR=/usr/local/nvm bash
 chmod -R a+rw /usr/local/nvm
 export NVM_DIR=/usr/local/nvm
 source /usr/local/nvm/nvm.sh
 
 
-nvm install v24.14.0
-npm install -g typescript
-nvm alias default node
+nvm install v24.14.0 >/dev/null 2>&1
+npm install -g typescript >/dev/null 2>&1
+nvm alias default node >/dev/null 2>&1
 nvm use default
 
 
