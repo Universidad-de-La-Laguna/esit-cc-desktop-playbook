158de2136f0a579fb6a59efcdd67a5481cfba2af
Pedro Gonzalez <pgonyan@gmail.com>
2026-03-23 13:10:29 +0000

    Add installation script for Python 3.9 and related packages

---
+++ no-ansible-scripts/install-software-byron.sh
@@ -0,0 +1,15 @@
+
+
+apt update
+apt install software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
+apt update
+apt install python3.9
+python3.9 --version
+
+apt install -y g++--12  pypy3
+
+
+update-alternatives --set javac /usr/lib/jvm/java-17-openjdk-amd64/bin/javac
+update-alternatives --set java  /usr/lib/jvm/java-17-openjdk-amd64/bin/java
+
