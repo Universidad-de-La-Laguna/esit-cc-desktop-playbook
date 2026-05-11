c6b335ba6e337d9bf363d0111266e0b2f1c1f16e
Pedro Gonzalez <pgonyan@gmail.com>
2026-05-11 12:41:00 +0100

    Fix install-code-restringido2.sh to correct install-extension command syntax

--- no-ansible-scripts/install-code-restringido2.sh
+++ no-ansible-scripts/install-code-restringido2.sh
@@ -4,6 +4,9 @@ echo ==========================================
 hostname
 echo ==========================================
 
+mkdir -p /opt/code-extensions
+cp -a /etc/esit-cc-desktop-playbook/no-ansible-scripts/code-extensions /opt/code-extensions
+
 cat > /usr/local/bin/code-restringido2 << 'EOF'
 DEST="/opt/vscode-fixed.v1.119"
 EXT="/opt/code-extensions"
@@ -13,14 +16,14 @@ cp $/EXT/settings.json  ~/.config/Code/settings.json
 
 cd $DEST
 $DEST/code \
---install-extensions $EXT/ms-vscode-remote.remote-ssh-0.122.0.vsix \
-$EXT/ms-vscode-remote.remote-ssh-edit-0.87.0.vsix \
-$EXT/dbaeumer.vscode-eslint-3.0.24.vsix \
-$EXT/esbenp.prettier-vscode-12.4.0.vsix \
-$EXT/mongodb.mongodb-vscode-1.16.0.vsix \
-$EXT/ms-vscode.live-server-0.4.18.vsix \
-$EXT/vitest.explorer-1.50.4.vsix \
-$EXT/postman.postman-for-vscode-1.19.1.vsix  \
+--install-extension $EXT/ms-vscode-remote.remote-ssh-0.122.0.vsix \
+--install-extension $EXT/ms-vscode-remote.remote-ssh-edit-0.87.0.vsix \
+--install-extension $EXT/dbaeumer.vscode-eslint-3.0.24.vsix \
+--install-extension $EXT/esbenp.prettier-vscode-12.4.0.vsix \
+--install-extension $EXT/mongodb.mongodb-vscode-1.16.0.vsix \
+--install-extension $EXT/ms-vscode.live-server-0.4.18.vsix \
+--install-extension $EXT/vitest.explorer-1.50.4.vsix \
+--install-extension $EXT/postman.postman-for-vscode-1.19.1.vsix  \
 && $DEST/code
 EOF
 
@@ -53,6 +56,6 @@ npm install -g typescript >/dev/null 2>&1
 nvm alias default node >/dev/null 2>&1
 nvm use default
 
-#====
+#==== 
 
 
